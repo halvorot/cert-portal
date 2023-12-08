95aef50afc740d96a9167b34a6f282a9be4c4b7a
TEIGEN Halvor <halvor.teigen@soprasteria.com>
2023-12-08 20:37:05 +0100

    Add motion to front page

--- src/components/CertificationsGrid.tsx
+++ src/components/CertificationsGrid.tsx
@@ -1,18 +1,39 @@
-"use client"
+"use client";
 import CertificationCard from "./CertificationCard";
 import { SimpleGrid } from "@chakra-ui/react";
 import { CertificationType } from "@/utils/types";
+import { motion } from "framer-motion";
 
 export default function CertificationGrid({
   certifications,
 }: {
   certifications: CertificationType[];
 }) {
+  const container = {
+    visible: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+
   return (
-    <SimpleGrid minChildWidth="18rem" spacing="20px">
-      {certifications.map((row) => (
-        <CertificationCard key={row.id} certification={row} />
-      ))}
-    </SimpleGrid>
+    <motion.ul variants={container} initial="hidden" animate="visible">
+      <SimpleGrid minChildWidth="18rem" spacing="20px">
+        {certifications.map((row) => (
+          <motion.li key={row.id} variants={item}>
+            <CertificationCard certification={row} />
+          </motion.li>
+        ))}
+      </SimpleGrid>
+    </motion.ul>
   );
 }
