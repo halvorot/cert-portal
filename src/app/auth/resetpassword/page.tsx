0db640226649703aaef659753f23f2eeb5d03fd5
TEIGEN Halvor <halvor.teigen@soprasteria.com>
2024-02-11 18:03:47 +0100

    fix

--- src/app/auth/resetpassword/page.tsx
+++ src/app/auth/resetpassword/page.tsx
@@ -2,9 +2,27 @@ import { readUserSession } from "@/utils/authUtils";
 import { redirect } from "next/navigation";
 import ResetPasswordForm from "@/components/ResetPasswordForm";
 import { Flex, Text, SlideFade } from "@chakra-ui/react";
+import { createSupabaseClient } from "@/utils/supabase/client";
 
-export default async function ResetPassword() {
+export default async function ResetPassword({
+  searchParams,
+}: {
+  searchParams: { code: string };
+}) {
+
+  if (searchParams.code) {
+    const supabase = createSupabaseClient();
+    const { error } = await supabase.auth.exchangeCodeForSession(searchParams.code);
+    if (error) {
+      return redirect(`/login?message=${error.message}`);
+    }
+  }
   const { data } = await readUserSession();
+
+  if (!data.session) {
+    const message = "Could not reset password, make sure you are using the correct link";
+    redirect(`/login?message=${message}`)
+  }
 
   return (
     <Flex width="100%" alignItems="center" justifyContent="center">
